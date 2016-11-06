4d4668cf1c0b0ba0feb7ee6290de7707a22e64af
BearJ <Bear156>
2016-11-06 13:09:04 +0800

    o

--- 大鸿汽配系统.cmd
+++ 大鸿汽配系统.cmd
@@ -1,5 +1,5 @@
-@echo ====================大鸿汽配系统正在运行，请缩小此窗口，不要关闭。====================
-git pull
+@echo ==========大鸿汽配系统正在运行，请缩小此窗口，不要关闭。==========
+
 mysqldump -u root -psa dahongcar >.\backup\%DATE:~0,4%%DATE:~5,2%%DATE:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%.sql
 start http://localhost:18080
 node app.js
